42bd5fa52b33da45125871b1c1af57ce2f694f9b
Robert Fisher <fisherro@users.noreply.github.com>
2026-04-13 07:16:30 -0500

    Refactor combinators.scm by cleaning up code
    Removed unused imports and commented code, streamlined combinator definitions.

--- combinators.scm
+++ combinators.scm
@@ -1,19 +1,11 @@
-;#lang racket/base
-;(require racket/set)
-;(require racket/function)
-;(require srfi/26)
 (import (scheme base)
-        (scheme case-lambda))
+        (scheme case-lambda)
+        (srfi 1))
+; (srfi 16)
 
 ;;; Inspired by
 ;;; Combinatory Logic and Combinators in Array Languages
 ;;; by Conor Hoekstra
-
-;(define-syntax-rule (define-combinator name (param ...) (arg ...) body)
-;  (define name
-;    (case-lambda
-;      ((param ...) (lambda (arg ...) body))
-;      ((param ... arg ...) ((name param ...) arg ...)))))
 
 (define-syntax define-combinator
   (syntax-rules ()
@@ -28,44 +20,27 @@
          ((param ...) (lambda rest body))
          ((param ... . rest) (apply (name param ...) rest))))]))
 
-#|
 (define-syntax-rule (show expr)
   (printf "~a: ~a~n" 'expr expr))
 
 ;; Combinators
 (define (I x) x)
-;(define (K x y) x)
 (define-combinator K (x) (y) x)
-; S is a specialization of phi
-; S is (also?) a specialization of D
 (define-combinator S (f g) (x) (f x (g x)))
 (define-combinator B (f g) (x) (f (g x)))
 (define-combinator B1 (f g) (x y) (f (g x y)))
-;(define B*
-;  (case-lambda
-;    ((f g) (lambda args (f (apply g args))))
-;    ((f g . args) (apply (B* f g) args))))
 (define-combinator B* (f g) args (f (apply g args)))
 (define-combinator C (f) (x y) (f y x))
-; W is a specialization of S
-; W is (also?) a specialization of psi
 (define-combinator W (f) (x) (f x x))
-; Psi is a specialization of D2
 (define-combinator psi (f g) (x y) (f (g x) (g y)))
 (define-combinator phi (f g h) (x) (g (f x) (h x)))
-; Phi1 is a specialization of D2
 (define-combinator phi1 (f g h) (x y) (g (f x y) (h x y)))
-; D is a specialization of D2
 (define-combinator D (f g) (x y) (f x (g y)))
 (define-combinator D2 (f g h) (x y) (g (f x) (h y)))
 
 ;; Helpers
 (define (sum lyst) (apply + lyst))
-;(define (string-reverse s) (list->string (reverse (string->list s))))
 (define string-reverse (B list->string (B reverse string->list)))
-;(define (string-sort s) (list->string (sort (string->list s) char<?)))
-;(define string-sort
-;  (B list->string (B (curryr sort char<?) string->list)))
 (define string-sort
   (B list->string (phi string->list sort (K char<?))))
 
@@ -91,8 +66,8 @@
 (show (isAnagram "owls" "slow"))
 (show (isAnagram "cats" "dogs"))
 
-;(define isDisjoint (phi1 '() equal? set-intersect))
-(define isDisjoint (B1 null? set-intersect))
+;(define isDisjoint (phi1 '() equal? lset-intersection))
+(define isDisjoint (B1 null? lset-intersection))
 (show (isDisjoint '(1 2) '(3 4 5)))
 (show (isDisjoint '(2 3) '(3 4 5)))
 
@@ -101,4 +76,3 @@
 ;(show (isPrefixOf "dog" "catch"))
 
 (show (S K K 10))
-|#
