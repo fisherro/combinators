6e42e81126ea17cf747eec18522e4d75acc61fbb
Robert Fisher <fisherro@users.noreply.github.com>
2026-04-13 13:42:58 +0000

    Fixed ordering of procedure parameters in combinators with three procedure parameters

--- combinators.scm
+++ combinators.scm
@@ -51,31 +51,31 @@
 (define-combinator C (f) (x y) (f y x))
 (define-combinator W (f) (x) (f x x))
 (define-combinator PSI (f g) (x y) (f (g x) (g y)))
-(define-combinator PHI (f g h) args (g (apply f args) (apply h args)))
+(define-combinator PHI (f g h) args (f (apply g args) (apply h args)))
 (define-combinator D (f g) (x y) (f x (g y)))
-(define-combinator D2 (f g h) (x y) (g (f x) (h y)))
+(define-combinator D2 (f g h) (x y) (f (g x) (h y)))
 
 ;; Helpers
 (define (sum lyst) (apply + lyst))
 (define string-reverse (B list->string (B reverse string->list)))
 (define string-sort
-  (B list->string (PHI string->list sort (K char<?))))
+  (B list->string (PHI sort string->list (K char<?))))
 (define intersect (cut lset-intersection equal? <> <>))
 
 ;; Examples
 (test (S K K 10) 10)
 
-(define avg (PHI sum / length))
+(define avg (PHI / sum length))
 (test (avg '(1 2 3 4)) 5/2)
 
-(define plus-or-minus (PHI + list -))
+(define plus-or-minus (PHI list + -))
 (test (plus-or-minus 10 5) '(15 5))
 
 (define absolute-difference (B abs -))
 (test (absolute-difference 10 7) 3)
 (test (absolute-difference 7 10) 3)
 
-(define palindrome1? (PHI string-reverse equal? I))
+(define palindrome1? (PHI equal? string-reverse I))
 (define palindrome2? (S equal? string-reverse))
 (test (palindrome1? "tacocat") #t)
 (test (palindrome2? "tacocat") #t)
